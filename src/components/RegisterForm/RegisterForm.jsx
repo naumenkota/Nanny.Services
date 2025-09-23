90411daedd14f12a68bb33b34fa5d8e47e64fbc9
Tetiana Naumenko <naumenko.ta@gmail.com>
2025-09-23 15:55:01 +0300

    add firebase to RegisterForm

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,12 +1,44 @@
 import s from "./RegisterForm.module.css";
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { auth, database } from "../../services/firebase.js";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { ref, set } from "firebase/database";
+
+const RegisterFormSchema = yup.object().shape({
+  name: yup.string().min(2).required("Name is required"),
+  email: yup.string().email("Invalid email").required("Email is required"),
+  password: yup.string().min(6).required("Password is required"),
+});
 
 export default function RegisterForm() {
-  const { register, handleSubmit } = useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     mode: "onChange",
+    resolver: yupResolver(RegisterFormSchema),
   });
-  const onSubmit = (data) => {
-    console.log(data);
+
+  const onSubmit = async (data) => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+
+      const user = userCredential.user;
+
+      await set(ref(database, `users/${user.uid}`), {
+        name: data.name,
+        email: data.email,
+      });
+    } catch (error) {
+      console.error("Registration error:", error.message);
+    }
   };
 
   return (
@@ -18,12 +50,20 @@ export default function RegisterForm() {
       </p>
       <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
         <input {...register("name")} placeholder="Name" className={s.input} />
+        {errors.name && <p className={s.error}>{errors.name.message}</p>}
+
         <input {...register("email")} placeholder="Email" className={s.input} />
+        {errors.email && <p className={s.error}>{errors.email.message}</p>}
+
         <input
           {...register("password")}
           placeholder="Password"
           className={s.input}
         />
+        {errors.password && (
+          <p className={s.error}>{errors.password.message}</p>
+        )}
+
         <button type="submit" className={s.btn}>
           Sign Up
         </button>
