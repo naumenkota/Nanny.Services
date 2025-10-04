218bbdf4695e82e83507bc0329e67df7c7e0e24e
Tetiana Naumenko <naumenko.ta@gmail.com>
2025-10-04 20:39:13 +0300

    add esc exit modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,6 +1,23 @@
 import s from "./Modal.module.css";
+import { useEffect } from "react";
 
 export default function Modal({ isOpen, onClose, children }) {
+  useEffect(() => {
+    if (!isOpen) return;
+
+    const handleEsc = (event) => {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    };
+
+    window.addEventListener("keydown", handleEsc);
+
+    return () => {
+      window.removeEventListener("keydown", handleEsc);
+    };
+  }, [isOpen, onClose]);
+
   if (!isOpen) return null;
 
   return (
