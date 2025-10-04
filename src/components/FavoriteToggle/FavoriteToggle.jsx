5a6c2a8123879c566d489e0ba83a8dd994aa06d9
Tetiana Naumenko <naumenko.ta@gmail.com>
2025-10-04 20:34:27 +0300

    fix favorite toggle

--- src/components/FavoriteToggle/FavoriteToggle.jsx
+++ src/components/FavoriteToggle/FavoriteToggle.jsx
@@ -3,29 +3,30 @@ import HeartIcon from "../../assets/heart.svg?react";
 import HeartPressedIcon from "../../assets/heartpressed.svg?react";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleFavorite } from "../../redux/favorites/favoritesSlice";
-import { useState } from "react";
+
 import Modal from "../Modal/Modal";
 import RegisterForm from "../RegisterForm/RegisterForm";
+import { openRegister } from "../../redux/modal/modalSlice.js";
 
 export default function FavoriteToggle({ nanny }) {
   const dispatch = useDispatch();
+
   const user = useSelector((state) => state.auth.user);
   const isLoading = useSelector((state) => state.auth.isLoading);
   const favorites = useSelector((state) => state.favorites.items);
-  const [registerOpen, setRegisterOpen] = useState(false);
-  console.log("FavoriteToggle user:", user, "isLoading:", isLoading);
+
   const isFavorite = favorites.some(
     (item) => item.uniqueKey === nanny.uniqueKey
   );
 
   const handleToggle = () => {
-    if (isLoading) return;
-
     if (!user?.uid) {
-      setRegisterOpen(true);
+      dispatch(openRegister());
       return;
     }
-    console.log("FavoriteToggle user:", user, "isLoading:", isLoading);
+
+    if (isLoading) return;
+
     dispatch(toggleFavorite({ nanny, uid: user.uid }));
   };
 
@@ -34,10 +35,6 @@ export default function FavoriteToggle({ nanny }) {
       <button className={s.btn} onClick={handleToggle}>
         {isFavorite ? <HeartPressedIcon /> : <HeartIcon />}
       </button>
-
-      <Modal isOpen={registerOpen} onClose={() => setRegisterOpen(false)}>
-        <RegisterForm onClose={() => setRegisterOpen(false)} />
-      </Modal>
     </>
   );
 }
